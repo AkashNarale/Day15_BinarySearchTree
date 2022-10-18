5e7c45a3b97d31125c1c033dfbecd61a9d020a71
AkashNarale <akashnarale43@gmail.com>
2022-10-18 18:00:06 +0530

    UC3

--- binaryUC3.cs
+++ binaryUC3.cs
@@ -1,28 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Day15_BinarySearchTree
 {
-    internal class binaryUC2<T> where T : IComparable
+    class binaryUC3<T> where T : IComparable<T>
     {
         public T NodeData
         {
             get; set;
         }
-        public binaryUC2<T> leftTree { get; set; }
-        public binaryUC2<T> rightTree { get; set; }
+        public binaryUC3<T> leftTree { get; set; }
+        public binaryUC3<T> rightTree { get; set; }
 
-        public binaryUC2(T nodeData)
+        public binaryUC3(T nodeData)
         {
             this.NodeData = nodeData;
             this.leftTree = null;
             this.rightTree = null;
         }
-        int leftCount = 0, rightCount = 0;
+
+        bool result = false;
         public void Insert(T item)
         {
             T currentNodeValue = this.NodeData;
@@ -30,7 +30,7 @@ namespace Day15_BinarySearchTree
             {
                 if (this.leftTree == null)
                 {
-                    this.leftTree = new binaryUC2<T>(item);
+                    this.leftTree = new binaryUC3<T>(item);
                 }
                 else
                     this.leftTree.Insert(item);
@@ -39,7 +39,7 @@ namespace Day15_BinarySearchTree
             {
                 if (this.rightTree == null)
                 {
-                    this.rightTree = new binaryUC2<T>(item);
+                    this.rightTree = new binaryUC3<T>(item);
                 }
                 else
                     this.rightTree.Insert(item);
@@ -49,19 +49,35 @@ namespace Day15_BinarySearchTree
         {
             if (this.leftTree != null)
             {
-                this.leftCount++;
                 this.leftTree.display();
             }
             Console.WriteLine(this.NodeData.ToString());
             if (this.rightTree != null)
             {
-                this.rightCount++;
                 this.rightTree.display();
             }
         }
-        public void size()
+
+        public bool IfExists(T element, binaryUC3<T> node)
         {
-            Console.WriteLine("\nSize of Binary Search Tree is" + " " + (1+ this.leftCount + this.rightCount));
+            if (node == null)
+                return false;
+            if (node.NodeData.Equals(element))
+            {
+                Console.WriteLine("Found the element in BST" + " " + node.NodeData);
+                result = true;
+            }
+            else
+                Console.WriteLine("Element is not present in Binary Search Tree");
+            if (element.CompareTo(node.NodeData) < 0)
+            {
+                IfExists(element, node.leftTree);
+            }
+            if (element.CompareTo(node.NodeData) > 0)
+            {
+                IfExists(element, node.rightTree);
+            }
+            return result;
         }
     }
 }
