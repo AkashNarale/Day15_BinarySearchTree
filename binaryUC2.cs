493c87122e42978fad8ec9c455e280292d4878fe
AkashNarale <akashnarale43@gmail.com>
2022-10-18 17:58:58 +0530

    UC2

--- binaryUC2.cs
+++ binaryUC2.cs
@@ -1,26 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Day15_BinarySearchTree
 {
-    internal class binaryUC1<T> where T : IComparable<T>
+    internal class binaryUC2<T> where T : IComparable
     {
         public T NodeData
         {
             get; set;
         }
-        public binaryUC1<T> leftTree { get; set; }
-        public binaryUC1<T> rightTree { get; set; }
+        public binaryUC2<T> leftTree { get; set; }
+        public binaryUC2<T> rightTree { get; set; }
 
-        public binaryUC1(T nodedata)
+        public binaryUC2(T nodeData)
         {
-            this.NodeData = nodedata;
-            this.rightTree = null;
+            this.NodeData = nodeData;
             this.leftTree = null;
+            this.rightTree = null;
         }
+        int leftCount = 0, rightCount = 0;
         public void Insert(T item)
         {
             T currentNodeValue = this.NodeData;
@@ -28,7 +30,7 @@ namespace Day15_BinarySearchTree
             {
                 if (this.leftTree == null)
                 {
-                    this.leftTree = new binaryUC1<T>(item);
+                    this.leftTree = new binaryUC2<T>(item);
                 }
                 else
                     this.leftTree.Insert(item);
@@ -37,7 +39,7 @@ namespace Day15_BinarySearchTree
             {
                 if (this.rightTree == null)
                 {
-                    this.rightTree = new binaryUC1<T>(item);
+                    this.rightTree = new binaryUC2<T>(item);
                 }
                 else
                     this.rightTree.Insert(item);
@@ -47,13 +49,19 @@ namespace Day15_BinarySearchTree
         {
             if (this.leftTree != null)
             {
-                this.leftTree.display();    
+                this.leftCount++;
+                this.leftTree.display();
             }
             Console.WriteLine(this.NodeData.ToString());
             if (this.rightTree != null)
             {
+                this.rightCount++;
                 this.rightTree.display();
             }
+        }
+        public void size()
+        {
+            Console.WriteLine("\nSize of Binary Search Tree is" + " " + (1+ this.leftCount + this.rightCount));
         }
     }
 }
